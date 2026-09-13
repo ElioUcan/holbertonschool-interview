54d1f4b868a125efc487dbf1dc41a11efafb14b5
ElioUcan <eliozapata2006@gmail.com>
2026-09-13 16:30:09 -0600

    Finished

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        row = [1]
+        for j in range(len(prev_row) - 1):
+            row.append(prev_row[j] + prev_row[j + 1])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
